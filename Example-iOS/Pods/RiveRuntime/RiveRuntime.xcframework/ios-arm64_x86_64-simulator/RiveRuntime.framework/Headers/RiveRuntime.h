230d459f8b607e98d48f8e7041d6becd2999fc64
Adam Trzcinski <trzcinski.adam@gmail.com>
2021-04-14 16:34:51 +0200

    feat: use new pod instead of framework

---
+++ Example-iOS/Pods/RiveRuntime/RiveRuntime.xcframework/ios-arm64_x86_64-simulator/RiveRuntime.framework/Headers/RiveRuntime.h
@@ -0,0 +1,18 @@
+//
+//  RiveRuntime.h
+//  RiveRuntime
+//
+//  Created by Matt Sullivan on 8/30/20.
+//  Copyright © 2020 Rive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RiveRuntime.
+FOUNDATION_EXPORT double RiveRuntimeVersionNumber;
+
+//! Project version string for RiveRuntime.
+FOUNDATION_EXPORT const unsigned char RiveRuntimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RiveRuntime/PublicHeader.h>
+#import <RiveRuntime/Rive.h>
