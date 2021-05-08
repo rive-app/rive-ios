63e7665a09fc7b38e5fbbbff936e1b79de90bd17
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-05-08 20:03:09 +0100

    removed riveViewController

--- Example-iOS/Source/UIkit/SimpleAnimation.swift
+++ Example-iOS/Source/UIkit/SimpleAnimation.swift
@@ -10,10 +10,15 @@ import UIKit
 import RiveRuntime
 
 
-class SimpleAnimationViewController: RiveViewController {
+class SimpleAnimationViewController: UIViewController {
     let resourceName = "truck_v7"
     
-    override func setRiveFile() -> RiveFile {
-        return getRiveFile(resourceName: resourceName)
+    
+    override public func loadView() {
+        super.loadView()
+        // Wire up an instance of RiveView to the controller
+        let view = RiveView()
+        self.view = view
+        (self.view! as! RiveView).configure(withRiveFile: getRiveFile(resourceName: resourceName))
     }
 }
