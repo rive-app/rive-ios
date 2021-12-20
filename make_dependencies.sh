ac6d5072558732deeaa66d2483f01495c3b67648
Luigi Rosso <luigi.rosso@gmail.com>
2021-12-20 15:54:46 -0800

    Fixing arm64 build.

--- make_dependencies.sh
+++ make_dependencies.sh
@@ -14,6 +14,18 @@ popd
 
 rm -fr dependencies
 mkdir dependencies
-cp -r submodules/rive-cpp/build/ios* dependencies
-cp -r submodules/rive-cpp/skia/renderer/build/ios* dependencies
+mkdir dependencies/debug
+mkdir dependencies/release
+
+cp -r submodules/rive-cpp/build/ios/bin/debug/librive.a dependencies/debug/librive.a
+cp -r submodules/rive-cpp/build/ios/bin/release/librive.a dependencies/release/librive.a
+cp -r submodules/rive-cpp/build/ios_sim/bin/debug/librive.a dependencies/debug/librive_sim.a
+cp -r submodules/rive-cpp/build/ios_sim/bin/release/librive.a dependencies/release/librive_sim.a
+
+cp -r submodules/rive-cpp/skia/renderer/build/ios/bin/debug/librive_skia_renderer.a dependencies/debug/librive_skia_renderer.a
+cp -r submodules/rive-cpp/skia/renderer/build/ios/bin/release/librive_skia_renderer.a dependencies/release/librive_skia_renderer.a
+cp -r submodules/rive-cpp/skia/renderer/build/ios_sim/bin/debug/librive_skia_renderer.a dependencies/debug/librive_skia_renderer_sim.a
+cp -r submodules/rive-cpp/skia/renderer/build/ios_sim/bin/release/librive_skia_renderer.a dependencies/release/librive_skia_renderer_sim.a
+
 cp -r submodules/rive-cpp/skia/dependencies/skia/out/libskia_ios.a dependencies
+cp -r submodules/rive-cpp/skia/dependencies/skia/out/libskia_ios_sim.a dependencies
