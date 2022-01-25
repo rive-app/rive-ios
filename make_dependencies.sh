87e6c2bfa8f18f24cab692f838bf55cad73ef591
Luigi Rosso <luigi.rosso@gmail.com>
2022-01-25 15:32:44 -0800

    use rive_skia_optimized

--- make_dependencies.sh
+++ make_dependencies.sh
@@ -27,5 +27,5 @@ cp -r submodules/rive-cpp/skia/renderer/build/ios/bin/release/librive_skia_rende
 cp -r submodules/rive-cpp/skia/renderer/build/ios_sim/bin/debug/librive_skia_renderer.a dependencies/debug/librive_skia_renderer_sim.a
 cp -r submodules/rive-cpp/skia/renderer/build/ios_sim/bin/release/librive_skia_renderer.a dependencies/release/librive_skia_renderer_sim.a
 
-cp -r submodules/rive-cpp/skia/dependencies/skia/out/libskia_ios.a dependencies
-cp -r submodules/rive-cpp/skia/dependencies/skia/out/libskia_ios_sim.a dependencies
+cp -r submodules/rive-cpp/skia/dependencies/skia_rive_optimized/out/libskia_ios.a dependencies
+cp -r submodules/rive-cpp/skia/dependencies/skia_rive_optimized/out/libskia_ios_sim.a dependencies
