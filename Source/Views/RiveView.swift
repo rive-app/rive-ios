bbc5d67970d63532af62732e1fac62a68dd8b337
Zachary Duncan <zachary.duncan@icloud.com>
2022-04-07 22:41:59 -0400

    UIKit implementation works now :)

--- Source/Views/RiveView.swift
+++ Source/Views/RiveView.swift
@@ -232,7 +232,7 @@ public class RiveView: RiveRendererView {
         stateChangeDelegate: RStateDelegate? = nil
     ) throws {
         super.init(frame: .zero)
-        let riveFile = try getRiveFile(resourceName: resource)
+        let riveFile = try RiveFile(name: resource)
         self.fit = fit
         self.alignment = alignment
         self.loopDelegate = loopDelegate
@@ -966,23 +966,4 @@ extension RiveView {
             eventQueue.add({ self.stopDelegate?.stop(stateMachine.name(), isStateMachine: true) })
         }
     }
-}
-
-
-func getBytes(resourceName: String, resourceExt: String=".riv") -> [UInt8] {
-    guard let url = Bundle.main.url(forResource: resourceName, withExtension: resourceExt) else {
-        fatalError("Failed to locate \(resourceName) in bundle.")
-    }
-    guard let data = try? Data(contentsOf: url) else {
-        fatalError("Failed to load \(url) from bundle.")
-    }
-    
-    // Import the data into a RiveFile
-    return [UInt8](data)
-}
-
-
-func getRiveFile(resourceName: String, resourceExt: String=".riv") throws -> RiveFile{
-    let byteArray = getBytes(resourceName: resourceName, resourceExt: resourceExt)
-    return try RiveFile(byteArray: byteArray)
 }
