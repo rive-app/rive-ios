df82ec86ebb7e50f14b8ba7a381b2aaa04f7944e
Zachary Duncan <zachary.duncan@icloud.com>
2022-04-08 12:02:00 -0400

    Added documentation to RModel

--- Source/Components/RModel.swift
+++ Source/Components/RModel.swift
@@ -9,24 +9,40 @@
 import Foundation
 
 public struct RModel { // TODO: Rename to RAssetState
+    /// The web address from which to load the `RiveFile`
     public let webURL: String? = nil
+    
+    /// The name of the file from which to load the .riv file
     public let fileName: String?
+    
+    /// Specifies how and if the animation should be resized to fit its container
     public var fit: RiveRuntime.Fit = .fitContain
+    
+    /// Specifies how the animation should be aligned to its container
     public var alignment: RiveRuntime.Alignment = .alignmentCenter
+    
+    /// Determines if the animation should play as soon as it is loaded
     public var autoplay: Bool = true
+    
+    /// Determines the `Artboard` to use. By default the first Artboard in the `RiveFile` is picked
     public var artboardName: String? = nil
+    
+    /// Determines the `Animation` to play. By default the first Animation/StateMachine in the `RiveFile` is picked
     public var animationName: String? = nil
+    
+    /// Determine the `StateMachine`to play, ignored if `animation` is set.
+    /// By default the first Animation/StateMachine in the riveFile is picked
     public var stateMachineName: String? = nil
     
     public var description: String {
           "URL: ["           + (webURL ?? "None")           + "]/n"
-        + "File Name: ["     + (fileName ?? "None")      + "]/n"
-        + "Fit: ["           + fit.description           + "]/n"
-        + "Alignment: ["     + alignment.description     + "]/n"
-        + "Autoplay: ["      + autoplay.description      + "]/n"
-        + "Artboard: ["      + (artboardName ?? "None")      + "]/n"
-        + "Animation: ["     + (animationName ?? "None")     + "]/n"
-        + "State Machine: [" + (stateMachineName ?? "None")  + "]/n"
+        + "File Name: ["     + (fileName ?? "None")         + "]/n"
+        + "Fit: ["           + fit.description              + "]/n"
+        + "Alignment: ["     + alignment.description        + "]/n"
+        + "Autoplay: ["      + autoplay.description         + "]/n"
+        + "Artboard: ["      + (artboardName ?? "None")     + "]/n"
+        + "Animation: ["     + (animationName ?? "None")    + "]/n"
+        + "State Machine: [" + (stateMachineName ?? "None") + "]/n"
     }
     
     public init(fileName: String?, stateMachineName: String? = nil) {
