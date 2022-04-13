d6c9b60bc57e9fc3941cacccb64b6863fcc0e130
Zachary Duncan <zachary.duncan@icloud.com>
2022-04-13 13:43:03 -0400

    Made the default RButton look more like a button

--- Example-iOS/Source/Widgets/RButton.swift
+++ Example-iOS/Source/Widgets/RButton.swift
@@ -10,23 +10,32 @@ import RiveRuntime
 import SwiftUI
 
 class RButton: RViewModel {
+    private let input = "IsPressed"
     var action: (() -> Void)? = nil
     
-    init(fileName: String) {
-        let model = RModel(fileName: fileName, fit: .fitCover, autoplay: false)
+    init(fileName: String = "rbutton") {
+        let model = RModel(fileName: fileName, stateMachineName: "State Machine 1", fit: .fitCover, autoplay: false)
         super.init(model)
     }
     
     func view(_ action: (() -> Void)?) -> some View {
         self.action = action
         return super.view()
-            .frame(width: 100, height: 20)
-            .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+            .frame(width: 100, height: 30)
+    }
+    
+    func touchBegan(onArtboard artboard: RiveArtboard?, atLocation location: CGPoint) {
+        stop()
+        try? setInput(input, value: true)
     }
     
     func touchEnded(onArtboard artboard: RiveArtboard?, atLocation location: CGPoint) {
         stop()
-        try? play(animationName: "Pull")
         action?()
+        touchCancelled(onArtboard: artboard, atLocation: location)
+    }
+    
+    func touchCancelled(onArtboard artboard: RiveArtboard?, atLocation location: CGPoint) {
+        try? setInput(input, value: false)
     }
 }
