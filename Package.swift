84e4c7b854c74aecbb73afdcb587c5dc33daae30
mikerreed <hello@rive.app>
2022-04-29 20:07:30 +0000

    Updating version files

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/2.0.8/RiveRuntime.xcframework.zip",
-            checksum: "5e4e8b410abaeaac656e1bcde64114b7c7130416ab508a92a99eba5b16b08142"
+            url: "https://github.com/rive-app/rive-ios/releases/download/2.0.9/RiveRuntime.xcframework.zip",
+            checksum: "38b230e1f5ea2eb46654fee1356288f6a0d053b90eabc0b6e49087b4e304097b"
         ),
     ]
 )
