deaebd213291e786006fd793ee67856b03dc942e
Zachary Duncan <zachary.duncan@icloud.com>
2022-06-07 23:55:04 -0400

    Corrected conflict

--- Example-iOS/Source/Examples/SwiftUI/SwiftMultipleAnimations.swift
+++ Example-iOS/Source/Examples/SwiftUI/SwiftMultipleAnimations.swift
@@ -19,11 +19,11 @@ struct SwiftMultipleAnimations: DismissableView {
         ScrollView{
             VStack {
                 Text("Square - go around")
-                RiveViewModel(fileName: fileName, animationName: "goaround", artboardName: "Square").view()
+                RiveViewModel(model(), animationName: "goaround", artboardName: "Square").view()
                     .frame(height:200)
                 
                 Text("Square - roll around")
-                RiveViewModel(fileName: fileName, animationName: "rollaround", artboardName: "Square").view()
+                RiveViewModel(model(), animationName: "rollaround", artboardName: "Square").view()
                     .frame(height:200)
                 
                 Text("Circle")
