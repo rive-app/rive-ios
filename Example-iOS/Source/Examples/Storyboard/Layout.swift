d892ae4743f5d950734b9b91863733abaa6ea601
Zachary Duncan <zachary.duncan@icloud.com>
2022-06-13 17:09:52 -0400

    Gave the Layout example a more natural iOS structure

--- Example-iOS/Source/Examples/Storyboard/Layout.swift
+++ Example-iOS/Source/Examples/Storyboard/Layout.swift
@@ -9,89 +9,51 @@
 import UIKit
 import RiveRuntime
 
-class LayoutView: UIView {
-    typealias ButtonAction = (String)->Void
-    var fitButtonAction: ButtonAction?
-    var alignmentButtonAction: ButtonAction?
-    
-    @IBOutlet weak var riveView: RiveView!
-    @IBAction func fitButtonTriggered(_ sender: UIButton) {
-        fitButtonAction?(sender.currentTitle!)
-    }
-    @IBAction func alignmentButtonTriggered(_ sender: UIButton) {
-        alignmentButtonAction?(sender.currentTitle!)
-    }
-}
-
 class LayoutViewController: UIViewController {
+    @IBOutlet weak var riveView: RiveView!
     var viewModel = RiveViewModel(fileName: "truck_v7")
     
-    override public func loadView() {
-        super.loadView()
-        
-        guard let layoutView = view as? LayoutView else {
-            fatalError("Could not find LayoutView")
-        }
-        
-        viewModel.setView(layoutView.riveView)
-        
-        func setFit(name:String) {
-            var fit: Fit = .fitContain
-            switch name {
-            case "Fill":
-                fit = .fitFill
-            case "Contain":
-                fit = .fitContain
-            case "Cover":
-                fit = .fitCover
-            case "Fit Width":
-                fit = .fitFitWidth
-            case "Fit Height":
-                fit = .fitFitHeight
-            case "Scale Down":
-                fit = .fitScaleDown
-            case "None":
-                fit = .fitNone
-            default:
-                fit = .fitContain
-            }
-            viewModel.fit = fit
-        }
-        
-        func setAlignmnet(name:String) { 
-            var alignment: Alignment = .alignmentCenter
-            switch name {
-            case "Top Left":
-                alignment = .alignmentTopLeft
-            case "Top Center":
-                alignment = .alignmentTopCenter
-            case "Top Right":
-                alignment = .alignmentTopRight
-            case "Center Left":
-                alignment = .alignmentCenterLeft
-            case "Center":
-                alignment = .alignmentCenter
-            case "Center Right":
-                alignment = .alignmentCenterRight
-            case "Bottom Left":
-                alignment = .alignmentBottomLeft
-            case "Bottom Center":
-                alignment = .alignmentBottomCenter
-            case "Bottom Right":
-                alignment = .alignmentBottomRight
-            default:
-                alignment = .alignmentCenter
-            }
-            viewModel.alignment = alignment
-        }
-        
-        layoutView.fitButtonAction = setFit
-        layoutView.alignmentButtonAction = setAlignmnet
+    override func viewDidLoad() {
+        viewModel.setView(riveView)
     }
     
-    override public func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        (view as! LayoutView).fitButtonAction = nil
-        (view as! LayoutView).alignmentButtonAction = nil
+    @IBAction func fitButtonTriggered(_ sender: UIButton) {
+        setFit(name: sender.currentTitle!)
+    }
+    
+    @IBAction func alignmentButtonTriggered(_ sender: UIButton) {
+        setAlignment(name: sender.currentTitle!)
+    }
+    
+    func setFit(name: String = "") {
+        var fit: Fit = .fitContain
+        switch name {
+        case "Fill": fit = .fitFill
+        case "Contain": fit = .fitContain
+        case "Cover": fit = .fitCover
+        case "Fit Width": fit = .fitFitWidth
+        case "Fit Height": fit = .fitFitHeight
+        case "Scale Down": fit = .fitScaleDown
+        case "None": fit = .fitNone
+        default: fit = .fitContain
+        }
+        viewModel.fit = fit
+    }
+    
+    func setAlignment(name: String = "") {
+        var alignment: Alignment = .alignmentCenter
+        switch name {
+        case "Top Left": alignment = .alignmentTopLeft
+        case "Top Center": alignment = .alignmentTopCenter
+        case "Top Right": alignment = .alignmentTopRight
+        case "Center Left": alignment = .alignmentCenterLeft
+        case "Center": alignment = .alignmentCenter
+        case "Center Right": alignment = .alignmentCenterRight
+        case "Bottom Left": alignment = .alignmentBottomLeft
+        case "Bottom Center": alignment = .alignmentBottomCenter
+        case "Bottom Right": alignment = .alignmentBottomRight
+        default: alignment = .alignmentCenter
+        }
+        viewModel.alignment = alignment
     }
 }
