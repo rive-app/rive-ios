9623029b0ca07bb542fe6c3ad688678397620cbd
Chris Dalton <chris@rive.app>
2023-09-08 08:26:00 -0600

    myplspatch

--- Example-iOS/Source/AppDelegate.swift
+++ Example-iOS/Source/AppDelegate.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import RiveRuntime
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        RenderContextManager.shared().defaultRenderer = RendererType.skiaRenderer
         return true
     }
 
