a946d5d480dfda4bbd168be4a7813b6f661024af
dskuza <hello@rive.app>
2025-05-28 16:43:05 +0000

    Updating version files

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "RiveRuntime",
-    platforms: [.iOS("14.0"), .visionOS("1.0"), .tvOS("16.0"), .macOS("13.1")],
+    platforms: [.iOS("14.0"), .visionOS("1.0"), .tvOS("16.0"), .macOS("13.1"), .macCatalyst("14.0")],
     products: [
         .library(
             name: "RiveRuntime",
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/6.8.2/RiveRuntime.xcframework.zip",
-            checksum: "21521301f151f2f6c1d73a9bb5fad7a5086d43d168933436eead8f3038ad7cc0"
+            url: "https://github.com/rive-app/rive-ios/releases/download/6.9.0/RiveRuntime.xcframework.zip",
+            checksum: "848f5629a89f693490045059e6598b67473eddf97c7777eb6d66ee8efb091e3d"
         )
     ]
 )
