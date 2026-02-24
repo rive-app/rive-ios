7d2d484dd9a36d691ae36b7d5dad1a0c96a2beca
Seyed Mojtaba Hosseini Zeidabadi <chenzook+github@gmail.com>
2026-02-24 12:32:58 +0330

    feat: enhance swift api usage and eliminate the force cast to RiveImageAsset

--- Example-iOS/Source/Examples/SwiftUI/SwiftSimpleAssets.swift
+++ Example-iOS/Source/Examples/SwiftUI/SwiftSimpleAssets.swift
@@ -13,7 +13,7 @@ struct SwiftSimpleAssets: DismissableView {
     var dismiss: () -> Void = {}
     @StateObject private var riveViewModel = RiveViewModel(fileName: "simple_assets", autoPlay: false, loadCdn: false, customLoader: { (asset: RiveFileAsset, data: Data, factory: RiveFactory) -> Bool in
         
-        if (asset is RiveImageAsset){
+        if let asset = asset as? RiveImageAsset {
             
             guard let url = (.main as Bundle).url(forResource: asset.uniqueName(), withExtension: "jpeg") else {
                 fatalError("Failed to locate '\(asset.uniqueName())' in bundle.")
@@ -21,7 +21,7 @@ struct SwiftSimpleAssets: DismissableView {
             guard let data = try? Data(contentsOf: url) else {
                 fatalError("Failed to load \(url) from bundle.")
             }
-            (asset as! RiveImageAsset).renderImage(
+            asset.renderImage(
                 factory.decodeImage(data)
             )
             return true;
